8c472eea2f4b3e42e3967e2f584ba40c97900034
agalarza <agustin.galarza96@gmail.com>
2022-01-27 15:33:46 -0300

    fix en scope usuario

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -9,40 +9,41 @@ const getUsuarios = (req, res) => {
     let desde = req.query.desde || 0;
     // busca y mapea los atributos marcados
     Usuario.find({},
-            "nombre apellido empresa email img role password cuit dni direccion telefono usuario")
+        "nombre apellido empresa email img role password cuit dni direccion telefono usuario")
         .skip(desde)
         .limit(15)
 
-    // ejecuta, puede tener un error manejado.
-    .exec((err, usuarios) => {
-        if (err) {
-            return res.status(500).json({
-                ok: false,
-                mensaje: "Error cargando usuarios",
-                errors: err,
-            });
-        }
-        // metodo count donde va contando usuarios simplemente muestra un int que se incrementa con cada nuevo registro
-        Usuario.count({}, (err, conteo) => {
-            res.status(200).json({
-                ok: true,
-                usuarios: usuarios,
-                total: conteo,
+        // ejecuta, puede tener un error manejado.
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    mensaje: "Error cargando usuarios",
+                    errors: err,
+                });
+            }
+            // metodo count donde va contando usuarios simplemente muestra un int que se incrementa con cada nuevo registro
+            Usuario.count({}, (err, conteo) => {
+                res.status(200).json({
+                    ok: true,
+                    usuarios: usuarios,
+                    total: conteo,
+                });
             });
         });
-    });
 };
 
 const addUsuarios = (req, res) => {
     // seteo el body que viaja en el request. Todos los campos required del modelo deben estar aca si no falla
     // esto se setea en postman. Al hacer la peticion post en el body tipo x-www-form-urlencoded.
     let body = req.body;
+    let usuario;
     Usuario.find({})
         .exec((err, usuarios) => {
 
             if (usuarios.length == 0) {
 
-                let usuario = new Usuario({
+                usuario = new Usuario({
                     nombre: body.nombre,
                     apellido: body.apellido,
                     empresa: body.empresa,
@@ -57,7 +58,7 @@ const addUsuarios = (req, res) => {
                     usuario: body.email,
                 });
             } else {
-                let usuario = new Usuario({
+                usuario = new Usuario({
                     nombre: body.nombre,
                     apellido: body.apellido,
                     empresa: body.empresa,
